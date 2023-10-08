db516fc480388207617a958b38a5ada865cca2a4
poligenius <marco.97marini@gmail.com>
2023-10-08 14:54:06 +0200

    updated resume page

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -20,15 +20,14 @@ const work = [
     summary: `PwC stands for PricewaterhouseCoopers, which is one of the world's leading professional services networks.
     It is a multinational firm that provides a wide range of services to clients,
     including auditing, consulting, tax advisory, and assurance services. PwC serves clients from
-    various industries, such as finance, technology, healthcare, consumer goods, and more. In Price I joined
-    the Data & Analytics team, working on a great variety of projects to:`,
+    various industries, such as finance, technology, healthcare, consumer goods, and more.
+    \nIn Price I joined the Data & Analytics team, working on a great variety of projects, here is a list of my favorite ones:`,
     highlights: [
-      'Redesign and orchestrate the client\'s Data pipelines.',
-      'Ingest large data flows.',
-      'Develop backend and frontend of different applications to compute key financial risk indicators.',
-      'Project clustering algorithms to spot users with anomalous accesses inside the client\'s systems.',
-      'Advice clients during the architectural decision phase of Machine Learning projects.',
-      'Hold many hours of lecture during a collaboration with H-Farm college teaching students the foundations of Spark, Big Data and SQL.',
+      'Managed Big Data flows for a financial client, from data ingestion to scheduling and data transformation, speeding up the build update time by approximately 50% and significantly reducing both computational costs and data storage space.',
+      'Developed a complex application that allows for the calculation and scheduling of risk indicators. I implemented the backend using Python and the frontend with React. This application integrates seamlessly with a forecasting model for these indicators, enabling the client to anticipate market trends and take proactive or corrective actions.',
+      'Oversaw the development of a clustering algorithm designed to identify users with anomalous permissions within the client’s database. This initiative resulted in reducing the auditor’s workload by more than fivefold and significantly improving the precision of anomalous user detection.',
+      'Adviced clients during the architectural decision phase of Machine Learning projects.',
+      'Hold many hours of lecture during a collaboration with H-Farm college teaching students the foundations of Spark, Big Data and Machine Learning.',
     ],
   },
 ];
